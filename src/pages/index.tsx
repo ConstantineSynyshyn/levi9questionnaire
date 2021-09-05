0e7a0cffc82e0cea4b1514fe4b27bcd9e3ca68ef
Kostiantyn Synyshyn <kostiantyn.synyshyn1@leaseplan.com>
2021-09-05 16:49:27 +0300

    spike: add e2e & integration & cypress-ct testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div> Home Page</div>;
+  return <div>Home Page</div>;
 };
 
 export default Home;
