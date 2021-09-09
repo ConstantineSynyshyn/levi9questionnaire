3c8455fde2fb7e46693082a2199dc17b3d1c696d
Kostiantyn Synyshyn <kostiantyn.synyshyn1@leaseplan.com>
2021-09-09 11:10:21 +0300

    feat: add new pages

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-import { questionsData } from "../../__mocks__/questionsMock";
+import Container from "@material-ui/core/Container";
+
+import { questionsWithOptionsData } from "../../__mocks__/questionsMock";
 
 import type { Page } from "../types/page";
 
-import Quiz from "../components/Quiz";
+import QuestionContainer from "../components/QuestionContainer";
 
 const QuizPage: Page = () => {
-  return <Quiz questionsData={questionsData} />;
+  return <QuestionContainer questionsData={questionsWithOptionsData} />;
 };
+
+QuizPage.getLayout = (page) => (
+  <Container maxWidth="md" disableGutters>
+    {page}
+  </Container>
+);
 
 export default QuizPage;
