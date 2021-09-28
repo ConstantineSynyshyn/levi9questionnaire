5c378f19e353b9f623f7ecc57f27dca935d2ad02
Kostiantyn Synyshyn <kostiantyn.synyshyn1@leaseplan.com>
2021-09-28 18:55:39 +0300

    tes: add quiz completion check

--- src/pages/congratulation.tsx
+++ src/pages/congratulation.tsx
@@ -1,22 +1,59 @@
 import React from "react"
+import { useRouter } from "next/router"
+import { GetServerSideProps } from "next"
 
 import Container from "@material-ui/core/Container"
 import Box from "@material-ui/core/Box"
+import Grid from "@material-ui/core/Grid"
+import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography"
 
 import { Page } from "@types/page"
+import { ROUTES } from "@constants/routes"
+import { getUserByEmail } from "@db/entities/User"
 
-const CongratulationPage: Page = () => {
+const CongratulationPage: Page<Props> = () => {
+  const router = useRouter()
+  const homeButtonClickHandler = () => router.push(ROUTES.INDEX)
   return (
     <Box component="div" p={2}>
       <Typography variant="h1" color="textPrimary" gutterBottom>
-        Congratulation, your test if done.
+        Congratulations, you have finished the test.
       </Typography>
-      <Typography variant="body1" color="textPrimary" gutterBottom>
-        if you want to ran it one more time - got to DB and remove answers
+      <Typography variant="h4" color="textPrimary" gutterBottom>
+        We will reach to you when we process your answers.
       </Typography>
+      <Grid container justifyContent="center" spacing={2}>
+        <Grid item xs={4}>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={homeButtonClickHandler}
+          >
+            Go home
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const currentUser = await getUserByEmail("")
+  const isQuizCompleted = Boolean(currentUser?.quizEndTime)
+  if (!isQuizCompleted) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
 
 CongratulationPage.getLayout = (page) => (
