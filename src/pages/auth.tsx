da3fc65bb7b4a1197d90b720547670902a5e95a3
Kostiantyn Synyshyn <synyshynconst@gmail.com>
2021-10-04 15:17:24 +0300

    tes: fix auth component

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,37 +1,53 @@
-import React from "react";
+import React from "react"
 
-import { getSession } from "next-auth/client";
-import { Page } from "../types/page";
-import { useRouter } from "next/router";
-import { Container } from "@material-ui/core";
-import AuthForm from "../components/AuthForm";
+import { getSession } from "next-auth/client"
+import { Page } from "../types/page"
+import { useRouter } from "next/router"
+import { Container, makeStyles } from "@material-ui/core"
+
+import AuthForm from "../components/AuthForm"
+import Skeleton from "@material-ui/lab/Skeleton"
+
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
 
 const AuthPage: Page = () => {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState(true)
+  const router = useRouter()
+  const classes = useStyles()
 
   React.useEffect(() => {
-    (async function tryLogin() {
-      const session = await getSession();
+    ;(async function tryLogin() {
+      const session = await getSession()
       if (session) {
-        router.replace("/");
+        router.replace("/")
       } else {
-        setIsLoading(false);
+        setIsLoading(false)
       }
-    })();
-  }, [router]);
+    })()
+  }, [router])
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return (
+      <div className={classes.container}>
+        <Skeleton variant="circle" width={40} height={40} />
+        <Skeleton animation="wave" width={480} height={480} />
+      </div>
+    )
   }
 
-  return <AuthForm />;
-};
+  return <AuthForm />
+}
 
 AuthPage.getLayout = (page) => (
   <Container maxWidth="md" disableGutters>
     {page}
   </Container>
-);
+)
 
-export default AuthPage;
+export default AuthPage
