32d983bd1538582eae8a4c093e1c20a618ecbf76
Kostiantyn Synyshyn <synyshynconst@gmail.com>
2021-10-04 14:32:21 +0300

    spike: fix hash encoding & env

--- src/services/RequestManager/handleAutoRegistration.ts
+++ src/services/RequestManager/handleAutoRegistration.ts
@@ -1,16 +1,16 @@
-import { hash } from "bcrypt";
+import { hash } from "bcrypt"
 
-import { getUserByEmail, userAutoRegistration } from "@db/entities/User";
-import { BASE_APP_URL } from "@constants/configuration";
-import { send } from "@lib/sendEmail";
+import { getUserByEmail, userAutoRegistration } from "@db/entities/User"
+import { BASE_APP_URL } from "@constants/configuration"
+import { send } from "@lib/sendEmail"
 
-const emailTitle = "Confirmation email for Levi9 testing platform registration";
+const emailTitle = "Confirmation email for Levi9 testing platform registration"
 
 const emailTemplate =
   "<h3>Wellcome to Levi9 testing platform</h3>" +
   "<p>You successfully regisetred in testing platform</p>" +
   "<p>To be able to run test, please confirm your email: " +
-  '<a href="{url}">{text}</a></p>';
+  '<a href="{url}">{text}</a></p>'
 
 /**
  * It is easier to implement to make that logic without next-auth,
@@ -19,27 +19,30 @@ const emailTemplate =
 const handleAutoRegistration = async (
   email: string
 ): Promise<{ success: any } | { error: string } | null> => {
-  const user = await getUserByEmail(email);
+  const user = await getUserByEmail(email)
   if (user?.isAdmin) {
     return Promise.resolve({
       error: "Email reserved. Try other one or connect to administrator",
-    });
+    })
   }
-  const confirmationHash = await hash(email + Date.now(), 12);
-  await userAutoRegistration(email, confirmationHash, Boolean(user));
-  const url = BASE_APP_URL + `registration/confirm/${confirmationHash}`;
-  const html = emailTemplate.replace("{url}", url).replace("{text}", url);
+  const confirmationHash = await hash(email + Date.now(), 12)
+  await userAutoRegistration(email, confirmationHash, Boolean(user))
+  const url =
+    BASE_APP_URL +
+    `registration/confirm/${encodeURIComponent(confirmationHash)}`
+
+  const html = emailTemplate.replace("{url}", url).replace("{text}", url)
   try {
-    const result = await send(email, emailTitle, html);
+    const result = await send(email, emailTitle, html)
     // @TODO should return real response
-    return Promise.resolve({ success: true });
+    return Promise.resolve({ success: true })
     // return Promise.resolve({ success: result });
   } catch {
     return Promise.resolve({
       error:
         "Something went wrong. Connect to admin to receive you link or try  again",
-    });
+    })
   }
-};
+}
 
-export { handleAutoRegistration };
+export { handleAutoRegistration }
